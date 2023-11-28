b597b3ebb97463fd980e4d850ceeeb1f08152d2b
RyuDaeHeon <107351748+RyuDaeHeon@users.noreply.github.com>
2023-11-29 04:13:49 +0900

    Update HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,36 +1,110 @@
 import 'package:flutter/material.dart';
 
-class Main extends StatelessWidget {
-  const Main({super.key});
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int selectedIndex = 0;
+
+  List<Map<String, dynamic>> icons = [
+    {'icon': Icons.home},
+    {'icon': Icons.chat},
+    {'icon': Icons.calendar_month},
+    {'icon': Icons.person},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'Home',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color(0xFF101522),
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-              height: 0.08,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          title: SizedBox.shrink(),
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.only(top: 30.0),
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/mainLogo.png'),
+                  fit: BoxFit.fill,
+                  alignment: Alignment.topCenter,
+                ),
+              ),
             ),
           ),
         ),
       ),
-      body: MainPanel(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Information',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Recommened Hospitals',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 70,
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  children: List.generate(icons.length, (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Container(
+                        color: selectedIndex == index ? Colors.grey[300] : Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              icons[index]['icon'],
+                              size: 30,
+                              color: selectedIndex == index ? Colors.lightGreen : Colors.black,
+                            ),
+                            SizedBox(height: 25),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-class MainPanel extends StatelessWidget {
-  const MainPanel({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
