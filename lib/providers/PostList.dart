43209497fcc763920b164efcadff2fab5ee01e08
zestbear <zestbear90@gmail.com>
2023-12-04 03:42:24 +0900

    전체 게시물 화면 디자인 수정 및 검색 기능 추가

--- lib/providers/PostList.dart
+++ lib/providers/PostList.dart
@@ -6,16 +6,34 @@ import 'Answer.dart';
 
 class PostList extends ChangeNotifier {
   List<Post> allPostList = [];
+  List<Post> searchPostList = [];
 
   List<Post> allNeurologyList = [];
   List<Post> allOrthopedicList = [];
   List<Post> allInternalMedicineList = [];
   List<Post> allDentistryList = [];
   List<Post> allTraumaList = [];
+  List<Post> allENTList = [];
+  List<Post> allObstetricsList = [];
+
+  List<Post> searchNeurologyList = [];
+  List<Post> searchOrthopedicList = [];
+  List<Post> searchInternalMedicineList = [];
+  List<Post> searchDentistryList = [];
+  List<Post> searchTraumaList = [];
+  List<Post> searchENTList = [];
+  List<Post> searchObstetricsList = [];
 
   // 모든 게시물 불러오기
   Future<void> updatePostList() async {
     allPostList = await postAPI.getPosts();
+    notifyListeners();
+  }
+
+  // 게시물 검색
+  Future<void> updateSearchPostList(String keyword) async {
+    searchPostList = await postAPI.getSearchPosts(keyword);
+    allPostList = searchPostList;
     notifyListeners();
   }
 
@@ -42,6 +60,59 @@ class PostList extends ChangeNotifier {
   // 외상 게시물 불러오기
   Future<void> updateTraumaPostList() async {
     allTraumaList = await postAPI.getCategoryPosts("Trauma");
+    notifyListeners();
+  }
+  // 이비인후과 게시물 불러오기
+  Future<void> updateENTPostList() async {
+    allENTList = await postAPI.getCategoryPosts("ENT");
+    notifyListeners();
+  }
+  // 산부인과 게시물 불러오기
+  Future<void> updateObstetricsPostList() async {
+    allObstetricsList = await postAPI.getCategoryPosts("Obstetrics");
+    notifyListeners();
+  }
+
+  // 신경과 게시물 검색
+  Future<void> searchNeurologyPostList(String keyword) async {
+    searchNeurologyList = await postAPI.getSearchCategoryPosts("Neurology", keyword);
+    allNeurologyList = searchNeurologyList;
+    notifyListeners();
+  }
+  // 정형외과 게시물 검색
+  Future<void> searchOrthopedicPostList(String keyword) async {
+    searchOrthopedicList = await postAPI.getSearchCategoryPosts("Orthopedic", keyword);
+    allOrthopedicList = searchOrthopedicList;
+    notifyListeners();
+  }
+  // 내과 게시물 검색
+  Future<void> searchInternalMedicinePostList(String keyword) async {
+    searchInternalMedicineList = await postAPI.getSearchCategoryPosts("InternalMedicine", keyword);
+    allInternalMedicineList = searchInternalMedicineList;
+    notifyListeners();
+  }
+  // 치의학 게시물 검색
+  Future<void> searchDentistryPostList(String keyword) async {
+    searchDentistryList = await postAPI.getSearchCategoryPosts("Dentistry", keyword);
+    allDentistryList = searchDentistryList;
+    notifyListeners();
+  }
+  // 외상 게시물 검색
+  Future<void> searchTraumaPostList(String keyword) async {
+    searchTraumaList = await postAPI.getSearchCategoryPosts("Trauma", keyword);
+    allTraumaList = searchTraumaList;
+    notifyListeners();
+  }
+  // 이비인후과 게시물 검색
+  Future<void> searchENTPostList(String keyword) async {
+    searchENTList = await postAPI.getSearchCategoryPosts("ENT", keyword);
+    allENTList = searchENTList;
+    notifyListeners();
+  }
+  // 산부인과 게시물 검색
+  Future<void> searchObstetricsPostList(String keyword) async {
+    searchObstetricsList = await postAPI.getSearchCategoryPosts("Obstetrics", keyword);
+    allObstetricsList = searchObstetricsList;
     notifyListeners();
   }
 
